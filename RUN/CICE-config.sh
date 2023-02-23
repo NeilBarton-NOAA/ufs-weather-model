a8dddebf00b6a1994ad35a8f819854093cfe3cc8
NeilBarton-NOAA <neil.barton@noaa.gov>
2023-02-23 13:45:39 +0000

    edits for CICE

--- RUN/CICE-config.sh
+++ RUN/CICE-config.sh
@@ -3,4 +3,9 @@ echo 'CICE-config.sh'
 ICE_tasks=${ICE_NMPI:-$ICE_tasks}
 NPROC_ICE=${ICE_tasks}
 atparse < ${PATHRT}/parm/ice_in_template > ice_in
+#TODO change output
+# sed
+#  histfreq       = 'm','d','h','x','x'
+#  histfreq_n     =  0 , 0 , 6 , 1 , 1
+
 
