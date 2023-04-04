0f8ebf0c5b3387915e553fb71291c1612362d5de
NeilBarton-NOAA <neil.barton@noaa.gov>
2023-04-04 18:17:07 +0000

    link files for GOCART upfront

--- RUN/GOCART-config.sh
+++ RUN/GOCART-config.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo 'GOCART-config.sh'
+GOCART_OPS=${GOCART_OPS:-F}
 
 ####################################
 # parse namelist
@@ -13,18 +14,17 @@ ln -sf ${INPUTDATA_ROOT}/GOCART/p8c_5d/ExtData .
 files=$(ls ${PATHRT}/parm/gocart/*.rc) 
 for f in ${files}; do
     if [[ ${FIX_METHOD} == 'LINK' ]]; then
-        LF+=(["${f}"]=".")
+        ln -s ${f} .
     else
         cp ${f} .
     fi
 done
 
-if [[ ${GOCART_OPS} == T ]];
+if [[ ${GOCART_OPS} == T ]]; then
 files="AERO_HISTORY.rc CAP.rc AERO_ExtData.rc DU2G_instance_DU.rc GOCART2G_GridComp.rc field_table"
 ${PATH_RUN}/FIX-from-hpss.sh GOCART_OPS ${NPB_FIX} 
 for f in ${files}; do
     rm ${f}
     cp ${NPB_FIX}/${f} .
 done
-
 fi
